1d6421bae914407078da219ab6000aef0e462d74
ke-zhang-rd <kezhang@bnl.gov>
2020-12-21 10:31:58 -0500

    MNT

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {'style_nav_header_background': '#ffd343'}
+html_theme_options = {'style_nav_header_background': '#ff8fbe'}
 import sphinx_rtd_theme
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
