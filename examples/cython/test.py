f6fc45f9c3ef9c2b23b7c9a11e2434c8ecaaffef
ke-zhang-rd <kz2249@columbia.edu>
2022-08-29 10:12:34 +0800

    MNT

---
+++ examples/cython/test.py
@@ -0,0 +1,10 @@
+import compute_wrapper
+import numpy as np
+
+x0, y0, x1, y1 = 1, 2, 3, 4
+rect_obj = compute_wrapper.PyRectangle(x0, y0, x1, y1)
+print(rect_obj.get_area())
+
+
+arr = np.array([1.0, 2.0, 3.0, 4.0], dtype='float')
+print(compute_wrapper.cal(arr))
