cd29828f44d58e873d9f3ae0519a31f3864747eb
ke-zhang-rd <kz2249@columbia.edu>
2022-08-29 09:46:00 +0800

    MNT

--- examples/cython/setup.py
+++ examples/cython/setup.py
@@ -5,14 +5,12 @@ import numpy
 # ext_modules = [Extension("rect.pyx", ["rectangle.cpp"], language='c++',)]
 
 setup(
-    ext_modules=cythonize(Extension("rect",
-                                    sources = ["rect.pyx"],
+    ext_modules=cythonize(Extension(name="compute_wrapper",
+                                    sources=["computer_wrapper.pyx"],
                                     include_dirs=[numpy.get_include()],
                                     language="c++",
                                     extra_compile_args=["-std=c++11"],
-                                    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-                          )
-                )
-
-
-)
+                                    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+                                   )
+                         )
+     )
